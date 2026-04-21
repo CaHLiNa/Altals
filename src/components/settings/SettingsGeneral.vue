025bb0c37ab6d28f2fab0bf43900214b73d87317
Feng Baobao <2114039928@qq.com>
2026-04-21 23:30:34 +0800

    fix(settings): relabel font group in general section

--- src/components/settings/SettingsGeneral.vue
+++ src/components/settings/SettingsGeneral.vue
@@ -1,7 +1,7 @@
 <!-- START OF FILE src/components/settings/SettingsGeneral.vue -->
 <template>
   <div class="general-page settings-page">
-    <h3 class="settings-section-title">{{ t('Fonts') }}</h3>
+    <h3 class="settings-section-title">{{ t('General') }}</h3>
 
     <section class="settings-group">
       <h4 class="settings-group-title">{{ t('Appearance') }}</h4>
@@ -58,7 +58,7 @@
     </section>
 
     <section class="settings-group">
-      <h4 class="settings-group-title">{{ t('General') }}</h4>
+      <h4 class="settings-group-title">{{ t('Fonts') }}</h4>
       <div class="settings-group-body">
         <div class="settings-row">
           <div class="settings-row-copy">
