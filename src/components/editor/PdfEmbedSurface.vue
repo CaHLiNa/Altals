0745962d7e51e92a0d066937bf50e7c92b1ca873
Feng Baobao <2114039928@qq.com>
2026-04-23 18:15:43 +0800

    refactor(latex): align forward sync with latex workshop

--- src/components/editor/PdfEmbedSurface.vue
+++ src/components/editor/PdfEmbedSurface.vue
@@ -316,8 +316,6 @@ async function handleForwardSyncLocation(detail = {}) {
       filePath: props.sourcePath,
       line,
       column,
-      semanticOrigin: String(detail.semanticOrigin || 'direct'),
-      semanticToken: String(detail.semanticToken || ''),
     },
   })
 }
